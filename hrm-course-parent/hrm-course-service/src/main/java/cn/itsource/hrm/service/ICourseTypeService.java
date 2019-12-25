fa283c5cca62019c578bd9f7529a115c03281992
solargen <lishugen@itsource.cn>
2019-12-25 15:36:22 +0800

    课程服务

---
+++ hrm-course-parent/hrm-course-service/src/main/java/cn/itsource/hrm/service/ICourseTypeService.java
@@ -0,0 +1,16 @@
+package cn.itsource.hrm.service;
+
+import cn.itsource.hrm.domain.CourseType;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 课程目录 服务类
+ * </p>
+ *
+ * @author solargen
+ * @since 2019-12-25
+ */
+public interface ICourseTypeService extends IService<CourseType> {
+
+}
