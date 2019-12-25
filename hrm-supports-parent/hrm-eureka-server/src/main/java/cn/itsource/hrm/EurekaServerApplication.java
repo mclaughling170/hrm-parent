59f52e1978bd8b7144678ec65e09324933626b8e
solargen <lishugen@itsource.cn>
2019-12-25 11:49:15 +0800

    搭建项目结构

---
+++ hrm-supports-parent/hrm-eureka-server/src/main/java/cn/itsource/hrm/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package cn.itsource.hrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description TODO
+ * @Author solargen
+ * @Date 2019/12/24 14:24
+ * @Version v1.0
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
---
+++ hrm-supports-parent/hrm-zuul-gateway/src/main/java/cn/itsource/hrm/ZuulGatewayApplication.java
@@ -0,0 +1,21 @@
+package cn.itsource.hrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Description TODO
+ * @Author solargen
+ * @Date 2019/12/24 14:33
+ * @Version v1.0
+ **/
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+
+}
